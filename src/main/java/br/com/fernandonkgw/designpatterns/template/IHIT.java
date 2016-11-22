bdac670da29c87175398b27cf198ef2d531b851f
fernandonkgw <fernandonkgw@gmail.com>
2016-11-21 23:57:07 -0200

    Criando imposto IHIT

---
+++ src/main/java/br/com/fernandonkgw/designpatterns/template/IHIT.java
@@ -0,0 +1,35 @@
+package br.com.fernandonkgw.designpatterns.template;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.fernandonkgw.designpatterns.Item;
+import br.com.fernandonkgw.designpatterns.Orcamento;
+
+public class IHIT extends TemplateDeImpostoCondicional {
+
+	@Override
+	protected double minimaTaxacao(Orcamento orcamento) {
+		return orcamento.getValor() * (0.01 * orcamento.getItens().size());
+	}
+
+	@Override
+	protected double maximaTaxacao(Orcamento orcamento) {
+		return orcamento.getValor() * 0.13 + 100;
+	}
+
+	@Override
+	protected boolean deveUsarMaximaTaxacao(Orcamento orcamento) {
+		
+		List<String> noOrcamento = new ArrayList<String>();
+		
+		for(Item item : orcamento.getItens()) {
+			if (noOrcamento.contains(item.getNome()))
+				return true;
+			else
+				noOrcamento.add(item.getNome());
+		}
+		return false;
+	}
+
+}
