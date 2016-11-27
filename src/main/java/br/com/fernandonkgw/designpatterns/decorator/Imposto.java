cb5384c959ef6b2984bbe7e8b4a47658fd6d6081
fernandonkgw <fernandonkgw@gmail.com>
2016-11-27 12:00:48 -0200

    Alterado o atributo outroImpostopara final

--- src/main/java/br/com/fernandonkgw/designpatterns/decorator/Imposto.java
+++ src/main/java/br/com/fernandonkgw/designpatterns/decorator/Imposto.java
@@ -3,19 +3,22 @@ package br.com.fernandonkgw.designpatterns.decorator;
 import br.com.fernandonkgw.designpatterns.Orcamento;
 
 public abstract class Imposto {
-	
-	protected Imposto outroImposto;
+
+	private final Imposto outroImposto;
 
 	public Imposto(Imposto outroImposto) {
 		this.outroImposto = outroImposto;
 	}
 
-	public Imposto() {}
-	
+	public Imposto() {
+		this.outroImposto = null;
+	}
+
 	public abstract double calcula(Orcamento orcamento);
-	
+
 	protected double calculoDoOutroImposto(Orcamento orcamento) {
-		if (outroImposto == null) 
+		// tratando o caso do próximo imposto nao existr
+		if (outroImposto == null)
 			return 0;
 		return outroImposto.calcula(orcamento);
 	}
